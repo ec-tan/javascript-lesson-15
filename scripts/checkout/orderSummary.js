7b5853bf1b9e3fc8d7013f3ff4ede4956a20c671
ec-tan <euchian.tan@gmail.com>
2024-05-02 16:36:14 +0100

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {products, getProduct} from '../../data/products.js'
 import {formatCurrency} from '../utils/money.js'
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js'
+import { renderPaymentSummary } from './paymentSummary.js'
 
 export function renderOrderSummary() {
 
@@ -105,13 +106,17 @@ export function renderOrderSummary() {
   document.querySelectorAll('.js-delete-link')
     .forEach((link) => {
       link.addEventListener('click', () => {
+        
         const productId = link.dataset.productId;
         removeFromCart(productId);
-
+        
         const container = document.querySelector(
           `.js-cart-item-container-${productId}`
         );
-        container.remove();
+        container.remove()
+
+        renderPaymentSummary()
+
       });
     });
 
@@ -121,6 +126,7 @@ export function renderOrderSummary() {
         const {productId, deliveryOptionId} = element.dataset
         updateDeliveryOption(productId, deliveryOptionId)
         renderOrderSummary()
+        renderPaymentSummary()
       })
     })
 }
